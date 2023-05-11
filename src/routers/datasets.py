7e2d6bbff6573fe26eac926c97cb9d45792279d2
PGijsbers <p.gijsbers@tue.nl>
2023-05-11 14:13:22 +0200

    refactor: re-use dataset example in old and new endpoint

--- src/routers/datasets.py
+++ src/routers/datasets.py
@@ -9,34 +9,35 @@ router = APIRouter(prefix="/datasets", tags=["datasets"])
 router_old_format = APIRouter(prefix="/old/datasets", tags=["datasets"])
 
 
+DATASET_EXAMPLE = {
+    "id": 1,
+    "name": "Anneal",
+    "version": 2,
+    "description": "The original Annealing dataset from UCI.",
+    "format": "ARFF",
+    "upload_date": "2014-04-06T23:19:20",
+    "licence": "Public",
+    "url": "https://www.openml.org/data/download/1/dataset_1_anneal.arff",
+    "file_id": 1,
+    "default_target_attribute": "class",
+    "version_label": "2",
+    "tag": [
+        "study_1",
+        "uci",
+    ],
+    "visibility": "public",
+    "original_data_url": "https://www.openml.org/d/2",
+    "status": "active",
+    "md5_checksum": "d01f6ccd68c88b749b20bbe897de3713",
+}
+
+
 @router.get(
     path="/{dataset_id}",
     description="Get meta-data for dataset with ID `dataset_id`.",
 )
 def get_dataset(_dataset_id: int) -> DatasetMetadata | DcatApWrapper:
-    example = DatasetMetadata.parse_obj(
-        {
-            "id": 1,
-            "name": "Anneal",
-            "version": 2,
-            "description": "The original Annealing dataset from UCI.",
-            "format": "ARFF",
-            "upload_date": "2014-04-06T23:19:20",
-            "licence": "Public",
-            "url": "https://www.openml.org/data/download/1/dataset_1_anneal.arff",
-            "file_id": 1,
-            "default_target_attribute": "class",
-            "version_label": "2",
-            "tag": [
-                "study_1",
-                "uci",
-            ],
-            "visibility": "public",
-            "original_data_url": "https://www.openml.org/d/2",
-            "status": "active",
-            "md5_checksum": "d01f6ccd68c88b749b20bbe897de3713",
-        },
-    )
+    example = DatasetMetadata.parse_obj(DATASET_EXAMPLE)
     return openml_dataset_to_dcat(example)
 
 
@@ -45,4 +46,4 @@ def get_dataset(_dataset_id: int) -> DatasetMetadata | DcatApWrapper:
     description="Get old-style wrapped meta-data for dataset with ID `dataset_id`.",
 )
 def get_dataset_wrapped(_dataset_id: int) -> dict[str, DatasetMetadata]:
-    return {"data_set_description": DatasetMetadata()}  # type: ignore[call-arg]
+    return {"data_set_description": DatasetMetadata.parse_obj(DATASET_EXAMPLE)}
