f0dd32285b391b00d25e049c1b2efee9d7636d2c
PGijsbers <p.gijsbers@tue.nl>
2023-05-26 11:55:37 +0200

    refactor: prefix private functions with an underscore

--- src/routers/datasets.py
+++ src/routers/datasets.py
@@ -32,18 +32,32 @@ class DatasetError(IntEnum):
     NO_DATA_FILE = 113
 
 
-def format_error(*, code: DatasetError, message: str) -> dict[str, str]:
+processing_info = namedtuple("processing_info", ["date", "warning", "error"])
+
+
+def _get_processing_information(dataset_id: int) -> processing_info:
+    """Return processing information, if any. Otherwise, all fields `None`."""
+    if not (data_processed := get_latest_processing_update(dataset_id)):
+        return processing_info(date=None, warning=None, error=None)
+
+    date_processed = data_processed["processing_date"]
+    warning = data_processed["warning"].strip() if data_processed["warning"] else None
+    error = data_processed["error"].strip() if data_processed["error"] else None
+    return processing_info(date=date_processed, warning=warning, error=error)
+
+
+def _format_error(*, code: DatasetError, message: str) -> dict[str, str]:
     """Formatter for JSON bodies of OpenML error codes."""
     return {"code": str(code), "message": message}
 
 
-def user_has_access(dataset: dict[str, Any], _user: Any) -> bool:
+def _user_has_access(dataset: dict[str, Any], _user: Any) -> bool:
     """Determine if `user` has the right to view `dataset`."""
     # TODO: when private, check if user is uploader or admin
     return cast(str, dataset["visibility"]) == Visibility.PUBLIC
 
 
-def format_parquet_url(dataset: dict[str, Any]) -> str | None:
+def _format_parquet_url(dataset: dict[str, Any]) -> str | None:
     if dataset["format"].lower() != DatasetFileFormat.ARFF:
         return None
 
@@ -51,17 +65,25 @@ def format_parquet_url(dataset: dict[str, Any]) -> str | None:
     return f"{minio_base_url}/dataset{dataset['did']}/dataset_{dataset['did']}.pq"
 
 
-def format_dataset_url(dataset: dict[str, Any]) -> str:
+def _format_dataset_url(dataset: dict[str, Any]) -> str:
     base_url = "https://test.openml.org"
     filename = f"{html.escape(dataset['name'])}.{dataset['format'].lower()}"
     return f"{base_url}/data/v1/download/{dataset['file_id']}/{filename}"
 
 
-def safe_unquote(text: str | None) -> str | None:
+def _safe_unquote(text: str | None) -> str | None:
     """Remove any open and closing quotes and return the remainder if non-empty."""
     if not text:
         return None
     return text.strip("'\"") or None
+
+
+def _csv_as_list(text: str | None, *, unquote_items: bool = True) -> list[str]:
+    """Return comma-separated values in `text` as list, optionally remove quotes."""
+    if not text:
+        return []
+    chars_to_strip = "'\"\t " if unquote_items else "\t "
+    return [item.strip(chars_to_strip) for item in text.split(",")]
 
 
 @router.get(
@@ -70,16 +92,16 @@ def safe_unquote(text: str | None) -> str | None:
 )
 def get_dataset(dataset_id: int) -> DatasetMetadata:
     if not (dataset := db_get_dataset(dataset_id)):
-        error = format_error(code=DatasetError.NOT_FOUND, message="Unknown dataset")
+        error = _format_error(code=DatasetError.NOT_FOUND, message="Unknown dataset")
         raise HTTPException(status_code=http.client.PRECONDITION_FAILED, detail=error)
 
     user = None  # get_user(...)
-    if not user_has_access(dataset, user):
-        error = format_error(code=DatasetError.NO_ACCESS, message="No access granted")
+    if not _user_has_access(dataset, user):
+        error = _format_error(code=DatasetError.NO_ACCESS, message="No access granted")
         raise HTTPException(status_code=http.client.PRECONDITION_FAILED, detail=error)
 
     if not (dataset_file := get_file(dataset["file_id"])):
-        error = format_error(
+        error = _format_error(
             code=DatasetError.NO_DATA_FILE,
             message="No data file found",
         )
@@ -87,7 +109,7 @@ def get_dataset(dataset_id: int) -> DatasetMetadata:
 
     tags = get_tags(dataset_id)
     description = get_latest_dataset_description(dataset_id)
-    processing_result = get_processing_information(dataset_id)
+    processing_result = _get_processing_information(dataset_id)
     status = get_latest_status_update(dataset_id)
 
     status_ = (
@@ -98,21 +120,14 @@ def get_dataset(dataset_id: int) -> DatasetMetadata:
     if description:
         description_ = description["description"].replace("\r", "").strip()
 
-    dataset_url = format_dataset_url(dataset)
-    parquet_url = format_parquet_url(dataset)
+    dataset_url = _format_dataset_url(dataset)
+    parquet_url = _format_parquet_url(dataset)
 
-    def csv_as_list(text: str | None, *, unquote_items: bool = True) -> list[str]:
-        """Return comma-separated values in `text` as list, optionally remove quotes."""
-        if not text:
-            return []
-        chars_to_strip = "'\"\t " if unquote_items else "\t "
-        return [item.strip(chars_to_strip) for item in text.split(",")]
-
-    contributors = csv_as_list(dataset["contributor"], unquote_items=True)
-    creators = csv_as_list(dataset["creator"], unquote_items=True)
-    ignore_attribute = csv_as_list(dataset["ignore_attribute"], unquote_items=True)
-    row_id_attribute = csv_as_list(dataset["row_id_attribute"], unquote_items=True)
-    original_data_url = csv_as_list(dataset["original_data_url"], unquote_items=True)
+    contributors = _csv_as_list(dataset["contributor"], unquote_items=True)
+    creators = _csv_as_list(dataset["creator"], unquote_items=True)
+    ignore_attribute = _csv_as_list(dataset["ignore_attribute"], unquote_items=True)
+    row_id_attribute = _csv_as_list(dataset["row_id_attribute"], unquote_items=True)
+    original_data_url = _csv_as_list(dataset["original_data_url"], unquote_items=True)
 
     # Not sure which properties are set by this bit:
     # foreach( $this->xml_fields_dataset['csv'] as $field ) {
@@ -138,7 +153,7 @@ def get_dataset(dataset_id: int) -> DatasetMetadata:
         description=description_,
         description_version=description["version"] if description else 0,
         tag=tags,
-        default_target_attribute=safe_unquote(dataset["default_target_attribute"]),
+        default_target_attribute=_safe_unquote(dataset["default_target_attribute"]),
         ignore_attribute=ignore_attribute,
         row_id_attribute=row_id_attribute,
         url=dataset_url,
@@ -151,20 +166,6 @@ def get_dataset(dataset_id: int) -> DatasetMetadata:
         collection_date=dataset["collection_date"],
         md5_checksum=dataset_file["md5_hash"],
     )
-
-
-processing_info = namedtuple("processing_info", ["date", "warning", "error"])
-
-
-def get_processing_information(dataset_id: int) -> processing_info:
-    """Return processing information, if any. Otherwise, all fields `None`."""
-    if not (data_processed := get_latest_processing_update(dataset_id)):
-        return processing_info(date=None, warning=None, error=None)
-
-    date_processed = data_processed["processing_date"]
-    warning = data_processed["warning"].strip() if data_processed["warning"] else None
-    error = data_processed["error"].strip() if data_processed["error"] else None
-    return processing_info(date=date_processed, warning=warning, error=error)
 
 
 @router_old_format.get(
