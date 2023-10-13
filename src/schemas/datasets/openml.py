11b4aa1d5bab462c747999430facf7ec40f1433e
PGijsbers <p.gijsbers@tue.nl>
2023-10-13 14:58:30 +0200

    Moved extra kwargs to json_schema_extra
    https://docs.pydantic.dev/2.4/migration/#changes-to-pydanticfield

--- src/schemas/datasets/openml.py
+++ src/schemas/datasets/openml.py
@@ -25,62 +25,81 @@ class DatasetStatus(StrEnum):
 
 
 class DatasetMetadata(BaseModel):
-    id_: int = Field(example=1, alias="id")
-    visibility: Visibility = Field(example=Visibility.PUBLIC)
-    status: DatasetStatus = Field(example=DatasetStatus.ACTIVE)
+    id_: int = Field(json_schema_extra={"example": 1}, alias="id")
+    visibility: Visibility = Field(json_schema_extra={"example": Visibility.PUBLIC})
+    status: DatasetStatus = Field(json_schema_extra={"example": DatasetStatus.ACTIVE})
 
-    name: str = Field(example="Anneal")
-    licence: str = Field(example="CC0")
-    version: int = Field(example=2)
+    name: str = Field(json_schema_extra={"example": "Anneal"})
+    licence: str = Field(json_schema_extra={"example": "CC0"})
+    version: int = Field(json_schema_extra={"example": 2})
     version_label: str = Field(
-        example="2",
-        description="Not sure how this relates to `version`.",
+        json_schema_extra={
+            "example": 2,
+            "description": "Not sure how this relates to `version`.",
+        },
     )
-    language: str = Field(example="English")
+    language: str = Field(json_schema_extra={"example": "English"})
 
     creators: list[str] = Field(
-        example=["David Sterling", "Wray Buntine"],
+        json_schema_extra={"example": ["David Sterling", "Wray Buntine"]},
         alias="creator",
     )
     contributors: list[str] = Field(
-        example=["David Sterling", "Wray Buntine"],
+        json_schema_extra={"example": ["David Sterling", "Wray Buntine"]},
         alias="contributor",
     )
-    citation: str = Field(example="https://archive.ics.uci.edu/ml/citation_policy.html")
-    paper_url: HttpUrl | None = Field(
-        example="http://digital.library.adelaide.edu.au/dspace/handle/2440/15227",
+    citation: str = Field(
+        json_schema_extra={"example": "https://archive.ics.uci.edu/ml/citation_policy.html"},
     )
-    upload_date: datetime = Field(example=datetime(2014, 4, 6, 23, 19, 20))
-    processing_date: datetime | None = Field(example=datetime(2019, 7, 9, 15, 22, 3))
+    paper_url: HttpUrl | None = Field(
+        json_schema_extra={
+            "example": "http://digital.library.adelaide.edu.au/dspace/handle/2440/15227",
+        },
+    )
+    upload_date: datetime = Field(json_schema_extra={"example": datetime(2014, 4, 6, 23, 19, 20)})
+    processing_date: datetime | None = Field(
+        json_schema_extra={"example": datetime(2019, 7, 9, 15, 22, 3)},
+    )
     processing_error: str | None = Field(
-        example="Please provide description XML.",
+        json_schema_extra={"example": "Please provide description XML."},
         alias="error",
     )
     processing_warning: str | None = Field(alias="warning")
-    collection_date: str | None = Field(example="1990")
+    collection_date: str | None = Field(json_schema_extra={"example": "1990"})
 
-    description: str = Field(example="The original Annealing dataset from UCI.")
-    description_version: int = Field(example=2)
-    tags: list[str] = Field(example=["study_1", "uci"], alias="tag")
-    default_target_attribute: str | None = Field(example="class")
-    ignore_attribute: list[str] | None = Field(example="sensitive_feature")
-    row_id_attribute: list[str] | None = Field(example="ssn")
+    description: str = Field(
+        json_schema_extra={"example": "The original Annealing dataset from UCI."},
+    )
+    description_version: int = Field(json_schema_extra={"example": 2})
+    tags: list[str] = Field(json_schema_extra={"example": ["study_1", "uci"]}, alias="tag")
+    default_target_attribute: str | None = Field(json_schema_extra={"example": "class"})
+    ignore_attribute: list[str] | None = Field(json_schema_extra={"example": "sensitive_feature"})
+    row_id_attribute: list[str] | None = Field(json_schema_extra={"example": "ssn"})
 
     url: HttpUrl = Field(
-        example="https://www.openml.org/data/download/1/dataset_1_anneal.arff",
-        description="URL of the main dataset data file.",
+        json_schema_extra={
+            "example": "https://www.openml.org/data/download/1/dataset_1_anneal.arff",
+            "description": "URL of the main dataset data file.",
+        },
     )
     parquet_url: HttpUrl | None = Field(
-        example="http://openml1.win.tue.nl/dataset2/dataset_2.pq",
-        description="URL of the parquet dataset data file.",
+        json_schema_extra={
+            "example": "http://openml1.win.tue.nl/dataset2/dataset_2.pq",
+            "description": "URL of the parquet dataset data file.",
+        },
     )
     minio_url: HttpUrl | None = Field(
-        example="http://openml1.win.tue.nl/dataset2/dataset_2.pq",
-        description="Deprecated, I think.",
+        json_schema_extra={
+            "example": "http://openml1.win.tue.nl/dataset2/dataset_2.pq",
+            "description": "Deprecated, I think.",
+        },
     )
-    file_id: int = Field(example=1)
-    format_: DatasetFileFormat = Field(example=DatasetFileFormat.ARFF, alias="format")
+    file_id: int = Field(json_schema_extra={"example": 1})
+    format_: DatasetFileFormat = Field(
+        json_schema_extra={"example": DatasetFileFormat.ARFF},
+        alias="format",
+    )
     original_data_url: list[HttpUrl] | None = Field(
-        example="https://www.openml.org/d/2",
+        json_schema_extra={"example": "https://www.openml.org/d/2"},
     )
-    md5_checksum: str = Field(example="d01f6ccd68c88b749b20bbe897de3713")
+    md5_checksum: str = Field(json_schema_extra={"example": "d01f6ccd68c88b749b20bbe897de3713"})
