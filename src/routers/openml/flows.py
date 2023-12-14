5e8e4fc37cee2b18c9e0fd1f468f331e92309622
PGijsbers <p.gijsbers@tue.nl>
2023-12-14 15:51:24 +0100

    Move database interactions to database submodule

--- src/routers/openml/flows.py
+++ src/routers/openml/flows.py
@@ -2,9 +2,11 @@ import http.client
 from typing import Annotated
 
 from core.conversions import _str_to_num
+from database.flows import get_flow as db_get_flow
+from database.flows import get_flow_parameters, get_flow_subflows, get_flow_tags
 from fastapi import APIRouter, Depends, HTTPException
 from schemas.flows import Flow, Parameter
-from sqlalchemy import Connection, text
+from sqlalchemy import Connection
 
 from routers.dependencies import expdb_connection
 
@@ -13,29 +15,11 @@ router = APIRouter(prefix="/flows", tags=["flows"])
 
 @router.get("/{flow_id}")
 def get_flow(flow_id: int, expdb: Annotated[Connection, Depends(expdb_connection)] = None) -> Flow:
-    flow_rows = expdb.execute(
-        text(
-            """
-            SELECT *, uploadDate as upload_date
-            FROM implementation
-            WHERE id = :flow_id
-            """,
-        ),
-        parameters={"flow_id": flow_id},
-    )
+    flow_rows = db_get_flow(flow_id, expdb)
     if not (flow := next(flow_rows, None)):
         raise HTTPException(status_code=http.client.NOT_FOUND, detail="Flow not found")
 
-    parameter_rows = expdb.execute(
-        text(
-            """
-            SELECT *, defaultValue as default_value, dataType as data_type
-            FROM input
-            WHERE implementation_id = :flow_id
-            """,
-        ),
-        parameters={"flow_id": flow_id},
-    )
+    parameter_rows = get_flow_parameters(flow_id, expdb)
     parameters = [
         Parameter(
             name=parameter.name,
@@ -49,28 +33,9 @@ def get_flow(flow_id: int, expdb: Annotated[Connection, Depends(expdb_connection
         for parameter in parameter_rows
     ]
 
-    tag_rows = expdb.execute(
-        text(
-            """
-            SELECT tag
-            FROM implementation_tag
-            WHERE id = :flow_id
-            """,
-        ),
-        parameters={"flow_id": flow_id},
-    )
-    tags = [tag.tag for tag in tag_rows]
+    tags = get_flow_tags(flow_id, expdb)
 
-    flow_rows = expdb.execute(
-        text(
-            """
-            SELECT child as child_id, identifier
-            FROM implementation_component
-            WHERE parent = :flow_id
-            """,
-        ),
-        parameters={"flow_id": flow_id},
-    )
+    flow_rows = get_flow_subflows(flow_id, expdb)
     subflows = [
         {
             "identifier": flow.identifier,
