0d541c3a85da903d5e32375cde517e5344ff56df
PGijsbers <p.gijsbers@tue.nl>
2023-12-15 14:41:26 +0100

    Move SQL queries to database submodule

--- src/routers/openml/study.py
+++ src/routers/openml/study.py
@@ -2,29 +2,23 @@ import http.client
 from typing import Annotated
 
 from core.formatting import _str_to_bool
-from database.studies import get_study_by_alias, get_study_by_id
+from database.studies import get_study_by_alias, get_study_by_id, get_study_data
 from database.users import User, UserGroup
 from fastapi import APIRouter, Depends, HTTPException
 from schemas.core import Visibility
 from schemas.study import Study, StudyType
-from sqlalchemy import Connection, text
+from sqlalchemy import Connection, Row
 
 from routers.dependencies import expdb_connection, fetch_user
 
 router = APIRouter(prefix="/studies", tags=["studies"])
 
 
-@router.get("/{study_id}")
-def get_study(
-    study_id: int | str,
-    user: Annotated[User | None, Depends(fetch_user)] = None,
-    expdb: Annotated[Connection, Depends(expdb_connection)] = None,
-) -> Study:
-    if isinstance(study_id, int) or study_id.isdigit():
-        study = get_study_by_id(int(study_id), expdb)
+def _get_study_raise_otherwise(id_or_alias: int | str, user: User | None, expdb: Connection) -> Row:
+    if isinstance(id_or_alias, int) or id_or_alias.isdigit():
+        study = get_study_by_id(int(id_or_alias), expdb)
     else:
-        study = get_study_by_alias(study_id, expdb)
-        study_id = study.id
+        study = get_study_by_alias(id_or_alias, expdb)
 
     if study is None:
         raise HTTPException(status_code=http.client.NOT_FOUND, detail="Study not found.")
@@ -39,36 +33,17 @@ def get_study(
             detail="Legacy studies are no longer supported",
         )
 
-    if study.type_ == StudyType.TASK:
-        study_data = expdb.execute(
-            text(
-                """
-                SELECT ts.task_id as task_id, ti.value as data_id
-                FROM task_study as ts LEFT JOIN task_inputs ti ON ts.task_id = ti.task_id
-                WHERE ts.study_id = :study_id AND ti.input = 'source_data'
-                """,
-            ),
-            parameters={"study_id": study_id},
-        ).fetchall()
-    else:
-        study_data = expdb.execute(
-            text(
-                """
-                SELECT
-                    rs.run_id as run_id,
-                    run.task_id as task_id,
-                    run.setup as setup_id,
-                    ti.value as data_id,
-                    setup.implementation_id as flow_id
-                FROM run_study as rs
-                JOIN run ON run.rid = rs.run_id
-                JOIN algorithm_setup as setup ON setup.sid = run.setup
-                JOIN task_inputs as ti ON ti.task_id = run.task_id
-                WHERE rs.study_id = :study_id AND ti.input = 'source_data'
-                """,
-            ),
-            parameters={"study_id": study_id},
-        ).fetchall()
+    return study
+
+
+@router.get("/{alias_or_id}")
+def get_study(
+    alias_or_id: int | str,
+    user: Annotated[User | None, Depends(fetch_user)] = None,
+    expdb: Annotated[Connection, Depends(expdb_connection)] = None,
+) -> Study:
+    study = _get_study_raise_otherwise(alias_or_id, user, expdb)
+    study_data = get_study_data(study, expdb)
     return Study(
         id_=study.id,
         name=study.name,
