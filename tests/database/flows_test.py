f5b609b2e42764779c21df541e3a01b506b2864a
PGijsbers <p.gijsbers@tue.nl>
2024-07-18 11:19:24 +0200

    Fix type issue

--- tests/database/flows_test.py
+++ tests/database/flows_test.py
@@ -6,6 +6,7 @@ from tests.conftest import Flow
 
 def test_database_flow_exists(flow: Flow, expdb_test: Connection) -> None:
     retrieved_flow = database.flows.get_by_name(flow.name, flow.external_version, expdb_test)
+    assert retrieved_flow is not None
     assert retrieved_flow.id == flow.id
     # when using actual ORM, can instead ensure _all_ fields match.
 
