406b8a23ac789f2c8fca3aca6d895a8641ea3a44
PGijsbers <p.gijsbers@tue.nl>
2024-10-09 20:53:24 +0200

    Remove infinite recursion from an indexed str being a str again

--- src/core/conversions.py
+++ src/core/conversions.py
@@ -15,18 +15,20 @@ def _str_to_num(string: str) -> int | float | str:
 def nested_str_to_num(obj: Any) -> Any:
     """Recursively tries to convert all strings in the object to numbers.
     For dictionaries, only the values will be converted."""
+    if isinstance(obj, str):
+        return _str_to_num(obj)
     if isinstance(obj, Mapping):
         return {key: nested_str_to_num(val) for key, val in obj.items()}
     if isinstance(obj, Iterable):
         return [nested_str_to_num(val) for val in obj]
-    if isinstance(obj, str):
-        return _str_to_num(obj)
     return obj
 
 
 def nested_num_to_str(obj: Any) -> Any:
     """Recursively tries to convert all numbers in the object to strings.
     For dictionaries, only the values will be converted."""
+    if isinstance(obj, str):
+        return obj
     if isinstance(obj, Mapping):
         return {key: nested_num_to_str(val) for key, val in obj.items()}
     if isinstance(obj, Iterable):
@@ -37,6 +39,8 @@ def nested_num_to_str(obj: Any) -> Any:
 
 
 def nested_remove_nones(obj: Any) -> Any:
+    if isinstance(obj, str):
+        return obj
     if isinstance(obj, Mapping):
         return {
             key: nested_remove_nones(val)
@@ -49,12 +53,12 @@ def nested_remove_nones(obj: Any) -> Any:
 
 
 def nested_remove_single_element_list(obj: Any) -> Any:
+    if isinstance(obj, str):
+        return obj
     if isinstance(obj, Mapping):
         return {key: nested_remove_single_element_list(val) for key, val in obj.items()}
     if isinstance(obj, Sequence):
         if len(obj) == 1:
             return nested_remove_single_element_list(obj[0])
-        if not obj:
-            return None
         return [nested_remove_single_element_list(val) for val in obj]
     return obj
