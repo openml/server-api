c7a220b9361e85ff8461edbff6461abf7475349e
PGijsbers <p.gijsbers@tue.nl>
2026-02-10 17:09:20 +0200

    Update test users with their new IDs

--- tests/users.py
+++ tests/users.py
@@ -4,12 +4,12 @@ from database.users import User, UserGroup
 
 NO_USER = None
 SOME_USER = User(user_id=2, _database=None, _groups=[UserGroup.READ_WRITE])
-OWNER_USER = User(user_id=16, _database=None, _groups=[UserGroup.READ_WRITE])
-ADMIN_USER = User(user_id=1, _database=None, _groups=[UserGroup.ADMIN, UserGroup.READ_WRITE])
+OWNER_USER = User(user_id=3229, _database=None, _groups=[UserGroup.READ_WRITE])
+ADMIN_USER = User(user_id=1159, _database=None, _groups=[UserGroup.ADMIN, UserGroup.READ_WRITE])
 
 
 class ApiKey(StrEnum):
     ADMIN = "abc"
-    SOME_USER = "normaluser"
-    OWNER_USER = "normaluser2"
+    SOME_USER = "normaluser2"
+    OWNER_USER = "normaluser"
     INVALID = "11111111111111111111111111111111"
