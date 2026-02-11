81b4f60e36ff486eaba561fd8b9dbb8ae2a33d47
PGijsbers <p.gijsbers@tue.nl>
2026-02-11 15:34:06 +0200

    make access safe even if toml doesn't have dev section

--- src/database/users.py
+++ src/database/users.py
@@ -10,7 +10,7 @@ from config import load_configuration
 # If `allow_test_api_keys` is set, the key may also be one of `normaluser`,
 # `normaluser2`, or `abc` (admin).
 api_key_pattern = r"^[0-9a-fA-F]{32}$"
-if load_configuration()["development"].get("allow_test_api_keys"):
+if load_configuration().get("development", {}).get("allow_test_api_keys"):
     api_key_pattern = r"^([0-9a-fA-F]{32}|normaluser|normaluser2|abc)$"
 
 APIKey = Annotated[
