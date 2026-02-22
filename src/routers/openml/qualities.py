696717a892f86035e52fc1f5db0654f2c829669e
Mahhheshh <100200105+Mahhheshh@users.noreply.github.com>
2026-02-22 16:03:48 +0530

    add extra error messages and fix response schema

--- src/routers/openml/qualities.py
+++ src/routers/openml/qualities.py
@@ -7,7 +7,7 @@ from sqlalchemy import Connection
 import database.datasets
 import database.qualities
 from core.access import _user_has_access
-from core.errors import DatasetError
+from core.errors import QualityError
 from database.users import User
 from routers.dependencies import expdb_connection, fetch_user
 from schemas.datasets.openml import Quality
@@ -32,18 +32,35 @@ def get_qualities(
     dataset_id: int,
     user: Annotated[User | None, Depends(fetch_user)],
     expdb: Annotated[Connection, Depends(expdb_connection)],
-) -> list[Quality]:
+) -> dict[Literal["data_qualities"], dict[Literal["quality"], list[Quality]]]:
     dataset = database.datasets.get(dataset_id, expdb)
     if not dataset or not _user_has_access(dataset, user):
         raise HTTPException(
             status_code=HTTPStatus.PRECONDITION_FAILED,
-            detail={"code": DatasetError.NO_DATA_FILE, "message": "Unknown dataset"},
+            detail={"code": QualityError.UNKNOWN_DATASET, "message": "Unknown dataset"},
         ) from None
-    return database.qualities.get_for_dataset(dataset_id, expdb)
-    # The PHP API provided (sometime) helpful error messages
-    # if not qualities:
-    # check if dataset exists: error 360
-    # check if user has access: error 361
-    # check if there is a data processed entry and forward the error: 364
-    # if nothing in process table: 363
-    # otherwise: error 362
+
+    processing = database.datasets.get_latest_processing_update(dataset_id, expdb)
+    if processing is None:
+        raise HTTPException(
+            status_code=HTTPStatus.PRECONDITION_FAILED,
+            detail={"code": QualityError.NOT_PROCESSED, "message": "Dataset not processed yet"},
+        )
+    if processing.error:
+        raise HTTPException(
+            status_code=HTTPStatus.PRECONDITION_FAILED,
+            detail={"code": QualityError.PROCESSED_WITH_ERROR, "message": "Dataset processed with error"},
+        )
+
+    qualities = database.qualities.get_for_dataset(dataset_id, expdb)
+    if not qualities:
+        raise HTTPException(
+            status_code=HTTPStatus.PRECONDITION_FAILED,
+            detail={"code": QualityError.NO_QUALITIES, "message": "No qualities found"},
+        )
+
+    return {
+        "data_qualities": {
+            "quality": qualities
+        }
+    }
