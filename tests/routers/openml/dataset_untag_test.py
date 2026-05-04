6d0bb5e774a3e0a7fdfff6066630aa0c4326e968
PGijsbers <p.gijsbers@tue.nl>
2026-05-04 14:54:05 +0200

    Add additional migration tests for untag

--- tests/routers/openml/dataset_untag_test.py
+++ tests/routers/openml/dataset_untag_test.py
@@ -10,6 +10,7 @@ The latter is more semantically correct, and is used for the Python tests.
 They share most of the underlying logic anyway.
 """
 
+import asyncio
 from collections.abc import Callable
 from contextlib import AbstractAsyncContextManager
 from http import HTTPStatus
@@ -133,3 +134,83 @@ async def test_dataset_untag_success_is_identical(
 
     assert py_response.status_code == php_response.status_code
     assert py_response.json() == php_response.json()
+
+
+@pytest.mark.mut
+async def test_dataset_untag_tag_does_not_exist_is_identical(
+    py_api: httpx.AsyncClient,
+    php_api: httpx.AsyncClient,
+) -> None:
+    dataset_id = 1
+    tag = "foo"
+
+    py_response, php_response = await asyncio.gather(
+        py_api.post(
+            f"/datasets/untag?api_key={ApiKey.OWNER_USER}", json={"tag": tag, "data_id": dataset_id}
+        ),
+        php_api.post(
+            f"/data/untag?api_key={ApiKey.OWNER_USER}", data={"tag": tag, "data_id": dataset_id}
+        ),
+    )
+
+    assert py_response.status_code == HTTPStatus.NOT_FOUND
+    assert php_response.status_code == HTTPStatus.PRECONDITION_FAILED
+    assert py_response.json()["code"] == php_response.json()["error"]["code"]
+    assert php_response.json()["error"]["message"] == "Tag not found."
+    assert tag in py_response.json()["detail"]
+    assert str(dataset_id) in py_response.json()["detail"]
+    assert "not found" in py_response.json()["detail"]
+
+
+@pytest.mark.mut
+async def test_dataset_untag_dataset_does_not_exist_is_identical(
+    py_api: httpx.AsyncClient,
+    php_api: httpx.AsyncClient,
+) -> None:
+    dataset_id = 9_999_999
+    tag = "foo"
+
+    py_response, php_response = await asyncio.gather(
+        py_api.post(
+            f"/datasets/untag?api_key={ApiKey.OWNER_USER}", json={"tag": tag, "data_id": dataset_id}
+        ),
+        php_api.post(
+            f"/data/untag?api_key={ApiKey.OWNER_USER}", data={"tag": tag, "data_id": dataset_id}
+        ),
+    )
+
+    assert py_response.status_code == HTTPStatus.NOT_FOUND
+    assert php_response.status_code == HTTPStatus.PRECONDITION_FAILED
+    assert py_response.json()["code"] == php_response.json()["error"]["code"]
+    assert php_response.json()["error"]["message"] == "Entity not found."
+    assert tag in py_response.json()["detail"]
+    assert str(dataset_id) in py_response.json()["detail"]
+    assert "not found" in py_response.json()["detail"]
+
+
+@pytest.mark.mut
+async def test_dataset_untag_tag_not_owned_is_identical(
+    py_api: httpx.AsyncClient,
+    php_api: httpx.AsyncClient,
+    temporary_tags: Callable[..., AbstractAsyncContextManager[None]],
+) -> None:
+    dataset_id = 1
+    tag = "foo"
+
+    async with temporary_tags(table="dataset_tag", tags=[tag], identifier=dataset_id, persist=True):
+        php_response = await php_api.post(
+            f"/data/untag?api_key={ApiKey.SOME_USER}", data={"tag": tag, "data_id": dataset_id}
+        )
+
+    async with temporary_tags(table="dataset_tag", tags=[tag], identifier=dataset_id):
+        py_response = await py_api.post(
+            f"/datasets/untag?api_key={ApiKey.SOME_USER}", json={"tag": tag, "data_id": dataset_id}
+        )
+
+    assert py_response.status_code == HTTPStatus.FORBIDDEN
+    assert php_response.status_code == HTTPStatus.PRECONDITION_FAILED
+    assert py_response.json()["code"] == php_response.json()["error"]["code"]
+    assert php_response.json()["error"]["message"] == "Tag is not owned by you"
+    assert tag in py_response.json()["detail"]
+    assert str(dataset_id) in py_response.json()["detail"]
+    assert "not allowed" in py_response.json()["detail"]
